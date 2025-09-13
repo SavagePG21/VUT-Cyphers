d6aa80718a899684e8ffcfddba1f8bf84cb5bef2
Kagiso <kagisogift291@outlook.com>
2025-09-13 14:13:51 +0200

    spring project

---
+++ src/cyphersbank/src/main/java/com/vutcyphers/cyphersbank/CyphersbankApplication.java
@@ -0,0 +1,13 @@
+package com.vutcyphers.cyphersbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyphersbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyphersbankApplication.class, args);
+    }
+
+}
